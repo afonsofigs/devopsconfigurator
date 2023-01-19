7a4ff6421c420e58a8195f8a7ffd5879a0a42f36
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-01-19 19:27:58 +0000

    gitignore change to fix gh-pages deployment

---
+++ build/_app/immutable/chunks/0-db6ac060.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-1b5e25a3.js";export{t as component};
---
+++ build/_app/immutable/chunks/3-067306a1.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/about/_page.svelte-f8938b06.js";export{t as component,r as universal};

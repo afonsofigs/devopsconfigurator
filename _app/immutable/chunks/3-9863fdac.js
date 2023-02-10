e367d463db0d7ebbbfb0d507868b8df733aeba04
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-02-10 01:44:43 +0000

    Updates

--- _app/immutable/chunks/3-9863fdac.js
+++ _app/immutable/chunks/3-9863fdac.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-90a7f2f4.js";import{default as t}from"../components/pages/about/_page.svelte-55281d6d.js";export{t as component,r as universal};
+import{_ as r}from"./_page-e94d8ac7.js";import{default as t}from"../components/pages/about/_page.svelte-55281d6d.js";export{t as component,r as universal};

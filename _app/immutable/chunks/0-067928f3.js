7565325ec73ec690ff2a4cae3eb6aed4b73bab0b
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-02-10 23:03:43 +0000

    Updates

---
+++ _app/immutable/chunks/0-067928f3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-adb7a007.js";export{t as component};
--- _app/immutable/chunks/3-9863fdac.js
+++ _app/immutable/chunks/3-9863fdac.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-e94d8ac7.js";import{default as t}from"../components/pages/about/_page.svelte-55281d6d.js";export{t as component,r as universal};
+import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/about/_page.svelte-55281d6d.js";export{t as component,r as universal};

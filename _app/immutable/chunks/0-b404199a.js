2a7d55e5cc06bf5b5ee5eec13beb28c7a48a4157
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-02-11 21:57:58 +0000

    Updates

---
+++ _app/immutable/chunks/0-b404199a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-7106c566.js";export{t as component};
---
+++ _app/immutable/chunks/stores-a8242a5c.js
@@ -0,0 +1,1 @@
+import"./index-e15c9d95.js";import{s as e}from"./singletons-429eb3a1.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};

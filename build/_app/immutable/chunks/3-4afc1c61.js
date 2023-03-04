0dda495e1a01bd1a67e26be54b7ee54f17663621
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-03-04 01:27:40 +0000

    Added dollar sign to final price, changed GitHub platforms from "Other" to "All" because it was confusing;

--- build/_app/immutable/chunks/3-4afc1c61.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/chats/_page.svelte-6e32f88d.js";export{t as component,r as universal};
--- src/lib/commons/options.js
+++ src/lib/commons/options.js
@@ -1,25 +1,25 @@
-export const gitPlatforms = ['Other', 'GitHub', 'Bitbucket', 'GitLab', 'Assembla'];
+export const gitPlatforms = ["All", "GitHub", "Bitbucket", "GitLab", "Assembla"];
 
-export const selfHosted = ['No', 'Both', 'Yes'];
+export const selfHosted = ["No", "Both", "Yes"];
 
-export const OSs = ['Linux', 'Docker', 'Windows', 'macOS'];
+export const OSs = ["Linux", "Docker", "Windows", "macOS"];
 
-export const boolJoins = ['Or', 'And'];
+export const boolJoins = ["Or", "And"];
 
-export const supports = ['External companies', 'No', 'Yes', 'Yes with SLA'];
+export const supports = ["External companies", "No", "Yes", "Yes with SLA"];
 
-export const ignoredCICDFieldsDefault = ['colID', 'ExtrasIncluded', 'LicenseCost$PerMonth', 'NCloudConcurrentBuilds',
-                                         'PriceExtraParallelBuild', 'NSelf-hostedRunners', 'PriceExtraRunners',
-                                         'BuildsPerMonthLimit', 'IncludedCreditsPerMonth',
-                                         'CreditsPerSelfHostedBuildMinute', 'CreditsPerCloudBuildMinuteLinux',
-                                         'CreditsPerCloudBuildMinuteDocker', 'CreditsPerCloudBuildMinuteWindows',
-                                         'CreditsPerCloudBuildMinutemacOS', 'PricePerExtraCredit', 'IncludedUsers',
-                                         'MaxUsers', 'PriceExtraUser$', 'IncludedCreditsPerExtraUser', 'EasyFill(1-3)'];
+export const ignoredCICDFieldsDefault = ["colID", "ExtrasIncluded", "LicenseCost$PerMonth", "NCloudConcurrentBuilds",
+                                         "PriceExtraParallelBuild", "NSelf-hostedRunners", "PriceExtraRunners",
+                                         "BuildsPerMonthLimit", "IncludedCreditsPerMonth",
+                                         "CreditsPerSelfHostedBuildMinute", "CreditsPerCloudBuildMinuteLinux",
+                                         "CreditsPerCloudBuildMinuteDocker", "CreditsPerCloudBuildMinuteWindows",
+                                         "CreditsPerCloudBuildMinutemacOS", "PricePerExtraCredit", "IncludedUsers",
+                                         "MaxUsers", "PriceExtraUser$", "IncludedCreditsPerExtraUser", "EasyFill(1-3)"];
 
-export const ignoredVCFieldsDefault = ['colID', 'LicenseCost$PerMonth', 'ExtrasIncluded', 'IncludedUsers', 'MaxUsers',
-                                       'PriceExtraUser$', 'DiskSpaceExtraUserGB', 'NPrivateReposPerExtraUser',
-                                       'PriceExtraGBDiskSpace', 'DiskSpaceGB', 'NPrivateReposIncluded',
-                                       'NPrivateReposFormula', 'DiskSpaceGBFormula'];
-export const ignoredChatFieldsDefault = ['colID', 'LicenseCost$PerMonth', 'ExtrasIncluded', 'IncludedUsers', 'MaxUsers',
-                                         'PriceExtraUser$', 'FreeTemporaryGuestsFormula', 'ChatFilesFormulaGB'];
+export const ignoredVCFieldsDefault = ["colID", "LicenseCost$PerMonth", "ExtrasIncluded", "IncludedUsers", "MaxUsers",
+                                       "PriceExtraUser$", "DiskSpaceExtraUserGB", "NPrivateReposPerExtraUser",
+                                       "PriceExtraGBDiskSpace", "DiskSpaceGB", "NPrivateReposIncluded",
+                                       "NPrivateReposFormula", "DiskSpaceGBFormula"];
+export const ignoredChatFieldsDefault = ["colID", "LicenseCost$PerMonth", "ExtrasIncluded", "IncludedUsers", "MaxUsers",
+                                         "PriceExtraUser$", "FreeTemporaryGuestsFormula", "ChatFilesFormulaGB"];
                         

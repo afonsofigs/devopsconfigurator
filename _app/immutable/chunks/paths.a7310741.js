a0aed895c7cb344d3e14affd5e8edb1a9e967892
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-06-06 19:30:50 +0100

    Updates

--- _app/immutable/chunks/paths.a7310741.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_g4qvg7)==null?void 0:s.base)??"/devopsconfigurator";var a;const t=((a=globalThis.__sveltekit_g4qvg7)==null?void 0:a.assets)??e;export{t as a,e as b};

eb27c07da8145be03894a87949c683c16be46e81
Afonso Figuinha <963669+afonsofigs@users.noreply.github.com>
2023-06-06 19:36:38 +0100

    Weights corrected

---
+++ build/_app/immutable/chunks/paths.c8ca12e2.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1srjzsp)==null?void 0:s.base)??"/devopsconfigurator";var a;const t=((a=globalThis.__sveltekit_1srjzsp)==null?void 0:a.assets)??e;export{t as a,e as b};
--- mcdm/main.py
+++ mcdm/main.py
@@ -40,15 +40,50 @@ def calculate(filename, excel_page, weights, filters):
 
 
 if __name__ == '__main__':
-    calculate("../src/lib/jsons/CIs.json", "CIs", [3, 5, 1, 5, 5, 3, 3, 3, 4, 2, 3, 5, 4],
-              ["mcdm_ExtrasIncluded", "mcdm_CD", "ExternalIntegrations", "mcdm_CloudOSs", "mcdm_SelfhostedOSs",
-               "mcdm_CloudConcurrent", "mcdm_SelfHostedConcurrent", "mcdm_BuildsLimit", "mcdm_NUsers", "mcdm_Caching",
-               "mcdm_ScheduledPipeline", "mcdm_Metrics", "mcdm_Support"])
+    CIsWeights = [2,  # mcdm_ExtrasIncluded-
+                  5,  # mcdm_CD.
+                  3.75,  # mcdm_GitPlatforms.
+                  2.5,  # ExternalIntegrations.
+                  4.5,  # mcdm_CloudOSs.
+                  4.5,  # mcdm_SelfhostedOSs.
+                  3.75,  # mcdm_CloudConcurrent.
+                  4,  # mcdm_SelfHostedConcurrent.
+                  3,  # mcdm_BuildsLimit-
+                  3,  # mcdm_NUsers-
+                  3.25,  # mcdm_Caching.
+                  3.75,  # mcdm_ScheduledPipeline.
+                  4.75,  # mcdm_Metrics.
+                  3.75  # mcdm_Support.
+                  ]
+    calculate("../src/lib/jsons/CIs.json", "CIs", CIsWeights,
+              ["mcdm_ExtrasIncluded", "mcdm_CD", "mcdm_GitPlatforms", "ExternalIntegrations", "mcdm_CloudOSs",
+               "mcdm_SelfhostedOSs", "mcdm_CloudConcurrent", "mcdm_SelfHostedConcurrent", "mcdm_BuildsLimit",
+               "mcdm_NUsers", "mcdm_Caching", "mcdm_ScheduledPipeline", "mcdm_Metrics", "mcdm_Support"])
 
-    calculate("../src/lib/jsons/VCs.json", "VCs", [2, 2, 2, 5, 4, 4, 2, 4],
-              ["mcdm_PackageRegistry", "mcdm_Wiki", "mcdm_Kanban", "mcdm_Issues", "mcdm_NUsers", "mcdm_NPrivateRepos",
-               "mcdm_DiskSpace", "mcdm_Support"])
+    VCsWeights = [2,  # mcdm_ExtrasIncluded-
+                  4,  # mcdm_Issues.
+                  3,  # mcdm_Kanban.
+                  3.5,  # mcdm_Wiki.
+                  3.25,  # mcdm_PackageRegistry.
+                  3,  # mcdm_NUsers-
+                  4.25,  # mcdm_NPrivateRepos.
+                  3.25,  # mcdm_DiskSpace.
+                  3.75  # mcdm_Support.
+                  ]
+    calculate("../src/lib/jsons/VCs.json", "VCs", VCsWeights,
+              ["mcdm_ExtrasIncluded", "mcdm_Issues", "mcdm_Kanban", "mcdm_Wiki", "mcdm_PackageRegistry", "mcdm_NUsers",
+               "mcdm_NPrivateRepos", "mcdm_DiskSpace", "mcdm_Support"])
 
-    calculate("../src/lib/jsons/Chats.json", "Chats", [2, 4, 3, 3, 1, 3, 4],
-              ["mcdm_ChatFiles", "mcdm_History", "mcdm_PerCall", "mcmd_CallDuration", "mcmd_FreeGuests",
-               "mcmd_Integrations", "mcdm_NUsers"])
+    ChatsWeights = [2,  # mcdm_ExtrasIncluded-
+                    3.25,  # mcdm_ChatFiles.
+                    4.25,  # mcdm_History.
+                    4,  # mcdm_PerCall.
+                    3.75,  # mcmd_CallDuration.
+                    3,  # mcmd_FreeGuests.
+                    4,  # mcmd_Integrations.
+                    3,  # mcdm_NUsers-
+                    3.75  # mcdm_Support.
+                    ]
+    calculate("../src/lib/jsons/Chats.json", "Chats", ChatsWeights,
+              ["mcdm_ExtrasIncluded", "mcdm_ChatFiles", "mcdm_History", "mcdm_PerCall", "mcmd_CallDuration",
+               "mcmd_FreeGuests", "mcmd_Integrations", "mcdm_NUsers", "mcdm_Support"])
